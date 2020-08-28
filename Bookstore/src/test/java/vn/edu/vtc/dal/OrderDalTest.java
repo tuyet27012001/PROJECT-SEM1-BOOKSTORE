1b40efb9c07ffe1f93fe689b870f4f65e417b50c
kien <mlqaqqgznihzc4@gmail.com>
2020-08-28 16:49:44 +0700

    kien push

--- Bookstore/src/test/java/vn/edu/vtc/dal/OrderDalTest.java
+++ Bookstore/src/test/java/vn/edu/vtc/dal/OrderDalTest.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 import org.junit.Test;
 
+import vn.edu.vtc.persistance.Book;
 import vn.edu.vtc.persistance.Order;
 
 public class OrderDalTest {
   OrderDal orderDal = new OrderDal();
 
   @Test
-  public void insertOrderTest() {
+  public void insertOrderTest1() {
     try {
       boolean result = orderDal.insertOrder("2020-8-10 12:52:03", 1, 1, 1, "Cho xac nhan");
       boolean expected = true;
@@ -42,4 +43,45 @@ public class OrderDalTest {
             // TODO: handle exception
         }
     }
+
+    @Test
+    public void orderListBookTest() {
+        try {
+            List<Book> listBook = orderDal.orderListBook(1);
+            assertNotNull(listBook);
+        } catch (final Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+    @Test
+    public void displayPaymentMethodsTest() {
+        try {
+            String a = orderDal.displayPaymentMethods();
+            assertNotNull(a);
+        } catch (final Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @Test
+    public void displayShippingUnitTest() {
+        try {
+            String a = orderDal.displayShippingUnit();
+            assertNotNull(a);
+        } catch (final Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @Test
+  public void insertBookOrderTest() {
+    try {
+      boolean result = orderDal.insertBookOrder(1, 1, 1, 97000);
+      boolean expected = true;
+      assertEquals(expected, result);
+    } catch (final Exception e) {
+      // TODO: handle exception
+    }
+  }
 }
